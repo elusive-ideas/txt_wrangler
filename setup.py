9def7ab6f94f385c559cf7c595f1f3b1e86cc592
David Martinez <david.martinez.anim@gmail.com>
2015-07-27 11:54:55 +0100

    Added setup.py for distribution

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='txtfile-wrangler',
+    version='0.1dev',
+    author="David Martinez",
+    author_email="david.martinez.anim@gmail.com",
+    packages=['txtfile-wrangler',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
