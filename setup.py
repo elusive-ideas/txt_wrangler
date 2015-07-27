f764bc425fa45683afbcceb808f29f8301a736c0
David Martinez <david.martinez.anim@gmail.com>
2015-07-27 12:19:40 +0100

    Changed name of the project and name of the package

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
 setup(
-    name='txtfile-wrangler',
+    name='txt-wrangler',
     version='0.1dev',
     author="David Martinez",
     author_email="david.martinez.anim@gmail.com",
-    packages=['txtfile-wrangler',],
+    packages=['txt_wrangler',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
 )
