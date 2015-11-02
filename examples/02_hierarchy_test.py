aedc53d24b1855a6db80604dcfbbeaff79f37d90
David Martinez <david.martinez.anim@gmail.com>
2015-11-02 00:14:50 +0000

    removed all data

--- examples/02_hierarchy_test.py
+++
@@ -1,8 +0,0 @@
-### Hierarchy of example.txt
-
-import txt_wrangler as tw
-
-root, sections = tw.get_hierarchy_from_file(file_name='D:\Python\examples\data\example.txt',
-                                            start_pattern='block_start',
-                                            end_pattern='block_end')
-print root
