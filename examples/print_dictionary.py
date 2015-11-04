3484b55e9f98bb613d1dfa9282f4c69e2a29c598
David Martinez <david.martinez.anim@gmail.com>
2015-11-04 14:56:51 +0000

    Updated

--- examples/print_dictionary.py
+++ examples/print_dictionary.py
@@ -4,13 +4,14 @@ Usage example for txt_wrangler.
 A text file is read and the contents are stored within a Python dictionary.
 Once the dictionary is retrieved, it is printed.
 '''
+
 import txt_wrangler as tw
 import os
 
 # Defines path to the main file to analyse as well as the modules path
 os.chdir(__file__.rpartition(os.sep)[0])
 main_file = r".\data\main.txt"
-tw.modules_folder = r'.\modules'
+tw.modules_folders = r'.\modules'
 
 # Reads the file using text_wrangler
 contents_dictionary = tw.read_file(main_file)
--- txt_wrangler/__init__.py
+++ txt_wrangler/__init__.py
@@ -2,15 +2,16 @@ import os.path
 import module_mgr as mm
 import txt_wrangler as tw
 
-modules_folder = None
+module_manager = mm.Manager()
+modules_folders = None
 
 
 def load_modules():
-    mm.register_paths([modules_folder])
-    registered_paths = mm.registered_paths()
-    mm.add_folders_to_pythonpath(registered_paths)
-    modules_discovered = mm.discover_modules()
-    modules_loaded = mm.load_modules(modules_discovered)
+    module_manager.register_paths([modules_folders])
+    registered_paths = module_manager.registered_paths()
+    module_manager.add_folders_to_pythonpath(registered_paths)
+    modules_discovered = module_manager.discover_modules()
+    modules_loaded = module_manager.load_modules(modules_discovered)
     return modules_loaded
 
 
