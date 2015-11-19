6f4951b866a7d0ffd3097764e39fe958206e5b72
Tomas Martinez <tmartinez@tmi-sl.com>
2015-11-19 20:10:59 +0100

    Program TEST OK after modified directory path

--- examples/print_dictionary.py
+++ examples/print_dictionary.py
@@ -8,10 +8,13 @@ Once the dictionary is retrieved, it is printed.
 import txt_wrangler as tw
 import os
 
+# Defines path to the modules to be used
+tw.file_folders = r'data'
+tw.modules_folders = r'modules'
+main_file = r'main.txt'
+
 # Defines path to the main file to analyse as well as the modules path
 os.chdir(__file__.rpartition(os.sep)[0])
-main_file = r".\data\main.txt"
-tw.modules_folders = r'.\modules'
 
 # Reads the file using text_wrangler
 contents_dictionary = tw.read_file(main_file)
